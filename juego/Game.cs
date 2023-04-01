4169f3052c58886d8e3282db788775ce0c4f332e
Carlos06929 <juanpi06929@gmail.com>
2023-04-01 08:49:57 -0400

    commit 1

---
+++ juego/Game.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace juego
+{
+    class Game:GameWindow
+    {
+
+        float[] vertices = {
+            -0.5f,-0.5f,0.0f,
+            0.5f,-0.5f,0.0f,
+            0.0f,0.5f,0.0f
+        };
+        int VertexBufferObject;
+        int VertexArrayObject;
+
+        Shader shader;
+
+        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { 
+            
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            KeyboardState input = Keyboard.GetState();
+            if (input.IsKeyDown(Key.Escape))
+            {
+                Exit();
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            // el codigo va aqui
+
+            VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer,
+                vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            //Al final se liberan todos los recursos utilizados pero si se quiere  eliminar de todas formas
+            //puede usarse el siguiente codigo
+            //GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            //GL.DeleteBuffer(VertexBufferObject);
+            VertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(VertexArrayObject);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+
+
+            shader = new Shader("../../shader.vert", "../../shader.frag");
+
+            shader.Use();
+            // 3. now draw the object
+            //someOpenGLFunctionThatDrawsOurTriangle();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            shader.Use();
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            //el codigo va aqui
+            Context.SwapBuffers();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, Width, Height);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            base.OnUnload(e);
+            shader.Dispose();
+        }
+
+
+    }
+}
---
+++ juego/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+
+
+
+namespace juego
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = new Game(800, 600, "LearnOpenTK"))
+            {
+                game.Run(60.0);
+
+                
+            }
+
+
+        }
+
+
+    }
+}
